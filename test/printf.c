ef321e12bebe0aa5561405197301172e9303c9e8
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-07-31 22:52:17 +0300

    Update printf.c

--- test/printf.c
+++ test/printf.c
@@ -1,32 +1,79 @@
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
-* _printf - function that prints based on format specifier
-* @format: takes in format specifier
-* Return: return pointer to index
-*/
+ * _putchar - Writes a single character to stdout.
+ * @c: The character to write.
+ *
+ * Return: On success, the number of characters written (1).
+ * On error, -1 is returned.
+ */
+int _putchar(char c) {
+    return write(1, &c, 1);
+}
 
-int _printf(const char *format, ...)
-{
-	va_list valist;
-	int count;
-	format_t get_opt[] = {
-		{"c", set_char},
-		{"s", set_string},
-		{"i", set_decimal},
-		{"d", set_decimal},
-		{"%", print_percent},
-		{NULL, NULL}
-	};
+/**
+ * _puts - Writes a string to stdout.
+ * @str: The string to write.
+ *
+ * Return: The number of characters written.
+ */
+int _puts(char *str) {
+    int len = 0;
 
-	if (!format)
-	{
-		return (-1);
-	}
+    if (str == NULL) {
+        str = "(null)";
+    }
 
-	va_start(valist, format);
-	count = parse_format(format, get_opt, valist);
-	va_end(valist);
+    while (*str) {
+        _putchar(*str++);
+        len++;
+    }
+    return len;
+}
 
-	return (count);
+/**
+ * _printf - Produces output according to a format.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed (excluding the null byte).
+ */
+int _printf(const char *format, ...) {
+    va_list args;
+    int i = 0, count = 0;
+    char *str;
+
+    if (format == NULL)
+        return (-1);
+
+    va_start(args, format);
+
+    while (format[i] != '\0') {
+        if (format[i] == '%') {
+            i++;
+            switch (format[i]) {
+                case 'c':
+                    count += _putchar(va_arg(args, int));
+                    break;
+                case 's':
+                    str = va_arg(args, char *);
+                    count += _puts(str);
+                    break;
+                case '%':
+                    count += _putchar('%');
+                    break;
+                default:
+                    count += _putchar('%');
+                    count += _putchar(format[i]);
+                    break;
+            }
+        } else {
+            count += _putchar(format[i]);
+        }
+        i++;
+    }
+
+    va_end(args);
+    return count;
 }
